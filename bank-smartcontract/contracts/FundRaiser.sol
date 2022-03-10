c10d9dd046d6bb909a72163fa62499d8fcd54b5d
techxyz <vivekbidla45@gmail.com>
2022-03-11 02:45:58 +0530

    hello

--- bank-smartcontract/contracts/FundRaiser.sol
+++ bank-smartcontract/contracts/FundRaiser.sol
@@ -3,44 +3,44 @@
 pragma solidity ^0.8.0;
 
 contract Bank {
-    address public bankOwner;
-    string public bankName;
-    mapping(address => uint256) public customerBalance;
+    address public companeyAddress;
+    string public companeyName;
+    mapping(address => uint256) public TotalDonation;
 
     constructor() {
-        bankOwner = msg.sender;
+        companeyAddress = msg.sender;
     }
 
     function depositMoney() public payable {
         require(msg.value != 0, "You need to deposit some amount of money!");
-        customerBalance[msg.sender] += msg.value;
+        TotalDonation[msg.sender] += msg.value;
     }
 
-    function setBankName(string memory _name) external {
+    function setcompaneyName(string memory _name) external {
         require(
-            msg.sender == bankOwner,
+            msg.sender == companeyAddress,
             "You must be the owner to set the name of the bank"
         );
-        bankName = _name;
+        companeyName = _name;
     }
 
     function withdrawMoney(address payable _to, uint256 _total) public {
         require(
-            _total <= customerBalance[msg.sender],
+            _total <= TotalDonation[msg.sender],
             "You have insuffient funds to withdraw"
         );
 
-        customerBalance[msg.sender] -= _total;
+        TotalDonation[msg.sender] -= _total;
         _to.transfer(_total);
     }
 
-    function getCustomerBalance() external view returns (uint256) {
-        return customerBalance[msg.sender];
+    function getTotalDonation() external view returns (uint256) {
+        return TotalDonation[msg.sender];
     }
 
     function getBankBalance() public view returns (uint256) {
         require(
-            msg.sender == bankOwner,
+            msg.sender == companeyAddress,
             "You must be the owner of the bank to see all balances."
         );
         return address(this).balance;
